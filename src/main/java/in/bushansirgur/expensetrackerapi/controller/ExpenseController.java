f9282e9367529eb4eb5e6a79eef22827d541be0e
naka-mb <wat.surf.learning789@gmail.com>
2023-10-18 21:28:20 +0900

    sec19

---
+++ src/main/java/in/bushansirgur/expensetrackerapi/controller/ExpenseController.java
@@ -0,0 +1,13 @@
+package in.bushansirgur.expensetrackerapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExpenseController {
+
+    @GetMapping(value = "/expenses")
+    public String getAllExpenses() {
+        return "List of Expenses";
+    }
+}
