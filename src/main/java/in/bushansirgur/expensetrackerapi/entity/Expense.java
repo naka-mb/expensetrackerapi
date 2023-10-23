b8d1faf46c492f15203a63fc4797aa39e054d650
naka-mb <wat.surf.learning789@gmail.com>
2023-10-23 14:04:55 +0900

    sec54

--- src/main/java/in/bushansirgur/expensetrackerapi/entity/Expense.java
+++ src/main/java/in/bushansirgur/expensetrackerapi/entity/Expense.java
@@ -1,6 +1,9 @@
 package in.bushansirgur.expensetrackerapi.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,11 +24,17 @@ public class Expense {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "expense_name")
+    @NotBlank(message = "Expense name must  not be null")
+    @Size(min=3,message="Expense name must be atleast 3 character")
     private String name;
     private String description;
     @Column(name = "expense_amount")
+    @NotNull(message = "Expense amount should not be null")
     private BigDecimal amount;
+
+    @NotBlank(message = "Catogory should not be null")
     private String category;
+    @NotNull(message = "Date must not be null")
     private Date date;
     @Column(name = "create_at",nullable = false,updatable = false)
     @CreationTimestamp
--- src/main/java/in/bushansirgur/expensetrackerapi/exceptions/GlobalExceptionHandler.java
+++ src/main/java/in/bushansirgur/expensetrackerapi/exceptions/GlobalExceptionHandler.java
@@ -1,16 +1,25 @@
 package in.bushansirgur.expensetrackerapi.exceptions;
 
 import in.bushansirgur.expensetrackerapi.entity.ErrorObject;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorObject> handleExpenseNotFoundException(ResourceNotFoundException ex, WebRequest request) {
@@ -19,5 +28,39 @@ public class GlobalExceptionHandler {
         errorObject.setMessage(ex.getMessage());
         errorObject.setTimestamp(new Date());
         return new ResponseEntity<>(errorObject,HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorObject> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex,WebRequest request) {
+        ErrorObject errorObject = new ErrorObject();
+        errorObject.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        errorObject.setMessage(ex.getMessage());
+        errorObject.setTimestamp(new Date());
+        return new ResponseEntity<>(errorObject,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorObject> handleGeneralException(Exception ex,WebRequest request) {
+        ErrorObject errorObject = new ErrorObject();
+        errorObject.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        errorObject.setMessage(ex.getMessage());
+        errorObject.setTimestamp(new Date());
+        return new ResponseEntity<>(errorObject,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        Map<String,Object> body = new HashMap<>();
+        body.put("timestamp",new Date());
+        body.put("statusCode",HttpStatus.BAD_REQUEST.value());
+        List<String> errors = ex.getBindingResult()
+                        .getFieldErrors()
+                        .stream()
+                        .map(x -> x.getDefaultMessage())
+                                .collect(Collectors.toList());
+
+        body.put("messages",errors);
+        return new ResponseEntity<>(body,HttpStatus.BAD_REQUEST);
     }
 }
